98468710c8be19ef2d2e63829b79689d2ec610e4
Shaaban78 <shabanumar78@gmail.com>
2023-07-13 19:01:56 -0700

    time to sleep

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - another one again
+ * Return 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+print("Size of a char: %lu bytes(s)\n", (unsigned long)
+sizofof(a));
+print("Size of an int: %lu bytes(s)\n", (unsigned long)
+sizofof(b));
+print("Size of a long int: %lu bytes(s)\n", (unsigned long)
+sizofof(c));
+print("Size of a long long int: %lu bytes(s)\n", (unsigned long)
+sizofof(d));
+print("Size of a float: %lu bytes(s)\n", (unsigned long)
+sizofof(f));
+return (0);
+}
+
