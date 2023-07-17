17da38f7d66f01a42d5061f134fb7d5609046020
Shaaban78 <shabanumar78@gmail.com>
2023-07-17 12:47:16 -0700

    try

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (success) 
-  */ 
-  
- int main(void) 
- { 
-         char alpha; 
-  
-         for (alpha = 'a'; alpha <= 'z'; alpha++) 
-         { 
-                 putchar(alpha); 
-         } 
-         putchar('\n'); 
-         return (0); 
+#include <stdio.h>
+
+  /**
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
+  */
+
+ int main(void)
+ {
+	 char alpha;
+	 
+	 for (alpha = 'a'; alpha <= 'z'; alpha++)
+	 {
+		 putchar(alpha);
+	 }
+	 putchar('\n');
+	 return (0);
  }
