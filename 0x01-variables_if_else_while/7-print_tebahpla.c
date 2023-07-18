28904f1cc55c2fa8d4acbf218cc6aec03021ec14
Shaaban78 <shabanumar78@gmail.com>
2023-07-17 18:37:09 -0700

    i have tried

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,19 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 0 (success) 
-  */ 
-  
- int main(void) 
- { 
-         char alpha; 
-  
-         for (alpha = 'z'; alpha >= 'a'; alpha--) 
-         { 
-                 putchar(alpha); 
-         } 
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
